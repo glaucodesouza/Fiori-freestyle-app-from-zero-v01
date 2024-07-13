d84aa5e253146b8c0f7d6b9846786456f059a3e0
glauco.r3 <glauco.r3@gmail.com>
2024-07-12 22:31:02 -0300

    first commit

---
+++ webapp/index.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+    // "sap/m/Text",
+    // "sap/m/Button"	
+
+], function(Text, Button) {
+    'use strict';
+    
+    // alert('Alo dentro do index.js !');
+
+    // new sap.m.Text({
+    //     text: 'Alo mundo pelo index.js!'
+    // }).placeAt("contentDoIndex");
+
+    // new Button({
+    //     text: "Me clica"
+    // }).placeAt("contentDoIndex");
+
+    // new Button({
+    //     text: "Nao me clica"
+    // }).placeAt("contentDoIndex");
+    
+});
